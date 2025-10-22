d3f592bff2fb048845a923c55742c20f30afb467
heavilydoped <45012674+heavilydoped1@users.noreply.github.com>
2025-10-22 08:40:24 +0300

    Create watchdog.py

---
+++ watchdog.py
@@ -0,0 +1,18 @@
+# watchdog.py
+import time
+import subprocess
+
+PATH = "/home/furkan/tubitak/send-signal.py"
+
+while True:
+    print("🚀 send-signal.py başlatılıyor...")
+    result = subprocess.run(["python3", PATH], capture_output=True, text=True)
+
+    print("⚠️ Kod çöktü veya kapandı.")
+    print("Çıktı:")
+    print(result.stdout)
+    print("Hata:")
+    print(result.stderr)
+
+    print("3 saniye sonra yeniden başlatılacak...\n")
+    time.sleep(3)
