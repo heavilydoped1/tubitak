b2bed563f08bdd3c16c3c3615fe2e049384b7927
heavilydoped <45012674+heavilydoped1@users.noreply.github.com>
2025-10-26 21:44:39 +0300

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -5,10 +5,10 @@ import subprocess
 PATH = "/home/furkan/tubitak/send-signal.py"
 
 while True:
-    print("🚀 send-signal.py başlatılıyor...")
+    print("send-signal.py başlatılıyor...")
     result = subprocess.run(["python3", PATH], capture_output=True, text=True)
 
-    print("⚠️ Kod çöktü veya kapandı.")
+    print("Kod çöktü veya kapandı.")
     print("Çıktı:")
     print(result.stdout)
     print("Hata:")
